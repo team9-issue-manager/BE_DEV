4e859f52976af2e55dcfd53f7dd006daded9d4f8
Yu Yunjae <yuyunjae@naver.com>
2024-05-12 21:53:25 +0900

    check push

---
+++ src/main/java/team9/issue_manage_system/IssueManageSystemApplication.java
@@ -0,0 +1,13 @@
+package team9.issue_manage_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueManageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueManageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team9/issue_manage_system/IssueManageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package team9.issue_manage_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
