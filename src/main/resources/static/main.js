6ca652280b2cf49d08e1ef117b2500e4a30295af
novaprospectCAU <novaprospect110a@gmail.com>
2024-05-24 12:11:59 +0900

    CSS파일과 버튼들을 추가했습니다. 아직 버튼들이 동작하지 않습니다.

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,16 +3,19 @@ function signUp() {
     submitButton.addEventListener("click", () => {
         const idValue = document.querySelector(".signUpID").value;
         const pwValue = document.querySelector(".signUpPW").value;
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:8080/userAdd", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log(this.responseText);
+        if (idValue.trim() !== "" && pwValue.trim() !== "") {
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", "http://localhost:8080/userAdd", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    console.log(this.responseText);
+                }
             }
+            const body = JSON.stringify({"id" : idValue, "password" : pwValue, "role" : "tester"});
+            xhr.send(body);
         }
-        const body = JSON.stringify({"id" : idValue, "password" : pwValue, "role" : "tester"});
-        xhr.send(body);
+
     })
 }
 
@@ -21,16 +24,18 @@ function signIn() {
     submitButton.addEventListener("click", () => {
         const idValue = document.querySelector(".signInID").value;
         const pwValue = document.querySelector(".signInPW").value;
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://localhost:8080/userFind", true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log(this.responseText);
+        if (idValue.trim() !== "" && pwValue.trim() !== "") {
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", "http://localhost:8080/userFind", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    console.log(this.responseText);
+                }
             }
+            const body = JSON.stringify({"id" : idValue, "password" : pwValue, "role" : "tester"});
+            xhr.send(body);
         }
-        const body = JSON.stringify({"id" : idValue, "password" : pwValue, "role" : "tester"});
-        xhr.send(body);
     })
 }
 
