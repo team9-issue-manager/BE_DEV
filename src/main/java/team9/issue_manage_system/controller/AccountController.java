a410546b0a81785d93493f494928a6e3c4b004d9
Yu Yunjae <yuyunjae@naver.com>
2024-05-27 14:56:05 +0900

    fix account_controller

--- src/main/java/team9/issue_manage_system/controller/AccountController.java
+++ src/main/java/team9/issue_manage_system/controller/AccountController.java
@@ -1,7 +1,6 @@
 package team9.issue_manage_system.controller;
 
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
