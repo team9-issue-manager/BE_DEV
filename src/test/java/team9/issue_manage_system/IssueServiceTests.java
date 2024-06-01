c8cee91c8b64f1c823c76381072e1ff887e43e59
yeahcold <roganyc@naver.com>
2024-06-01 21:41:51 +0900

    [feat] IssueServiceTests 구현

--- src/test/java/team9/issue_manage_system/IssueServiceTests.java
+++ src/test/java/team9/issue_manage_system/IssueServiceTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import team9.issue_manage_system.dto.IssueAssignDevDto;
 import team9.issue_manage_system.dto.IssueCreateDto;
 import team9.issue_manage_system.dto.IssueReturnDto;
@@ -23,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class IssueServiceTests {
 
